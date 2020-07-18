56d37fb40607abf24201667d177c47d44e6e5bf6
Ido Nov <idonov8@gmail.com>
2020-07-19 01:12:31 +0300

    show basic info about recipe on click

--- static/js/dynamic.js
+++ static/js/dynamic.js
@@ -1,31 +1,44 @@
-$(document).ready(function() {
+$(document).ready(() => {
 
+    const recipeTemplate = '' +
+        '<h3 class="recipeTitle" data-id={{id}}> {{title}} </h3>' +
+        '<div class="recipeContent" id={{id}} style="display: none;">' +
+        '<img src={{image}} width=300>' +
+        '<h4> There are <strong> {{missedIngredientCount}} </strong> additional ingredients that you will need' +
+        '</div>';
+    
+    const $recipes = $('#recipes')
 	$('form').on('submit', (event) => {
-		$.ajax({
-			data : {
-				ingredients : $('#ingredients').val()
-			},
-			type : 'POST',
-            url : '/getRecipe',
+
+        $.ajax({
+            data: {
+                ingredients: $('#ingredients').val()
+            },
+            type: 'POST',
+            url: '/getRecipe',
             dataType: 'json',
-		})
-		.done((data) => {
-           if (data.error) {
-                $('#errorAlert').text(data.error).show();
-                $('#recipes').hide()
-			} else {
-                $('#recipes').empty();
-                $.each(data.recipes, (index, element) => {
-                    $('#recipes').append($('<h3>', {text: element.title}),'</h3>');
-                });
-                $('#errorAlert').hide()
-                console.log(data)
-            }	 		
-		});
 
-        $('#recipes').text("Loading...").show();
-		event.preventDefault();
+        })
+            .done((data) => {
+                if (data.error) {
+                    $('#errorAlert').text(data.error).show();
+                    $recipes.hide()
+                } else {
+                    $recipes.empty();
+                    $.each(data.recipes, (index, element) => {
+                        $recipes.append(Mustache.render(recipeTemplate, element));
+                    });
+                    $recipes.slideDown();
+                    $('#errorAlert').hide()
+                    console.log(data)
+                }
+            });
+        $recipes.text("Loading...").show();
+        event.preventDefault();
+    });
 
-	});
-
+    $recipes.delegate('.recipeTitle','click',function() {
+        console.log("clicked", $(this));
+        $('#' + $(this).attr('data-id')).toggle(1000);
+    });
 });
