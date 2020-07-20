476f0e61ab0e2f0551140e80a6aec4365eb65ce6
Ido Nov <idonov8@gmail.com>
2020-07-20 07:25:17 +0300

    fix threading issue by using scoped_sesssion

--- db/database.py
+++ db/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 engine = create_engine('sqlite:///db/cookStuff.db')
 Base = declarative_base()
+session = scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-DBSession = sessionmaker(bind=engine, autoflush=False)
-session = DBSession()
